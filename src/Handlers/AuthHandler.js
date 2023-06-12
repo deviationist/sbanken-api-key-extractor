4c81fb2b4667a4d5750f04505f943ec20b4fba19
Robert S <robert4832@pm.me>
2023-06-12 13:28:39 +0200

    Fix broken authentication

--- src/Handlers/AuthHandler.js
+++ src/Handlers/AuthHandler.js
@@ -28,26 +28,25 @@ export default class AuthHandler {
 
             // Click "BankID"
             await this.page.click('.list.list--description > *:nth-child(2)');
-            await naturalDelay();
 
             // Fill social security number and submit
             await this.page.type('input[name="Ssn.Value"]', process.env.SOCIAL_SECURITY_NUMBER);
-            await naturalDelay();
             await this.page.click('input[type="submit"]');
+
+            // Wait for BankID-page to load
+            await this.page.waitForURL('https://login.bankid.no/');
+
+            // Click "BankID with app"
+            await this.page.getByRole('button', { name: 'BankID with app' }).click();
     
             // Wait for BankID-frame to appear
             await this.page.waitForSelector('iframe[title="BankID"]');
-    
-            // Locate frame
-            const frame = this.page.frameLocator('[title="BankID"]');
-    
-            // Input password and submit
-            const passwordField = frame.getByLabel('Ditt BankID-passord');
-            await passwordField.fill(process.env.BANKID_PASSWORD);
-            await naturalDelay();
 
-            // Submit - we should be logged in by now!
-            await frame.getByRole('button', { name: 'Neste' }).click();
+            // Fill password
+            await this.page.frameLocator('[title="BankID"]').getByLabel('Your BankID password').fill(process.env.BANKID_PASSWORD);
+
+            // Submit
+            await this.page.frameLocator('[title="BankID"]').getByRole('button', { name: 'Next' }).click();
         }
         if (!await this.wasLoggedIn()) {
             throw new Error('Login failed!');
